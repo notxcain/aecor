018f8c54aa3a8f4c96ae44151ff4b46b7904bfaf
Denis Mikhaylov <notxcain@gmail.com>
2016-06-23 11:38:57 +0300

    Format

--- core/src/main/scala/aecor/core/entity/CommandHandler.scala
+++ core/src/main/scala/aecor/core/entity/CommandHandler.scala
@@ -2,16 +2,20 @@ package aecor.core.entity
 
 import aecor.core.concurrent.Deferred
 
-
 sealed trait CommandHandlerResult[+E, +R]
+
 object CommandHandlerResult {
   case class Accept[+E](event: E) extends CommandHandlerResult[E, Nothing]
   case class Reject[+R](rejection: R) extends CommandHandlerResult[Nothing, R]
   case class Defer[E, R](f: Deferred[CommandHandlerResult[E, R]]) extends CommandHandlerResult[E, R]
-  
+
   def accept[E, R](event: E): CommandHandlerResult[E, R] = Accept(event)
   def reject[E, R](rejection: R):CommandHandlerResult[E, R] = Reject(rejection)
   def defer[E, R](f: Deferred[CommandHandlerResult[E, R]]): CommandHandlerResult[E, R] = Defer(f)
+}
+
+trait CommandHandler[-State, -Command, +Event, +Rejection] {
+  def apply(state: State, command: Command): CommandHandlerResult[Event, Rejection]
 }
 
 object CommandHandler {
@@ -19,8 +23,4 @@ object CommandHandler {
     new CommandHandler[State, Command, Event, Rejection] {
       override def apply(state: State, command: Command): CommandHandlerResult[Event, Rejection] = f(state)(command)
     }
-}
-
-trait CommandHandler[-State, -Command, +Event, +Rejection] {
-  def apply(state: State, command: Command): CommandHandlerResult[Event, Rejection]
 }
