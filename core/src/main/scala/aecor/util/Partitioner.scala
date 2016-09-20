fe7a8d176e119a960db0f371fbe8b111b7670168
Denis Mikhaylov <notxcain@gmail.com>
2016-09-20 14:14:40 +0300

    Remove deprecated methods

--- core/src/main/scala/aecor/util/Partitioner.scala
+++ core/src/main/scala/aecor/util/Partitioner.scala
@@ -4,8 +4,6 @@ import scala.util.hashing.MurmurHash3
 
 object Partitioner {
   def murmur3: Partitioner = new Murmur3Partitioner
-  @deprecated("Use murmur3 instead", "forever")
-  def paritionForString(string: String, partitionCount: Int): Int = scala.math.abs(MurmurHash3.stringHash(string)) % partitionCount
 }
 
 trait Partitioner {
