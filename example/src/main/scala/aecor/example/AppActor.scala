97af734de345f2aedbe32887f620dbceb7ba54ba
Denis Mikhaylov <notxcain@gmail.com>
2016-12-08 17:33:24 +0300

    Remove aecor-api

--- example/src/main/scala/aecor/example/AppActor.scala
+++ example/src/main/scala/aecor/example/AppActor.scala
@@ -2,7 +2,6 @@ package aecor.example
 
 import java.time.Clock
 
-import aecor.api.Router.ops._
 import aecor.core.aggregate._
 import aecor.core.streaming._
 import aecor.example.domain.CardAuthorization.CardAuthorizationCreated
@@ -37,61 +36,80 @@ class AppActor extends Actor with ActorLogging {
 
   val queries = new CassandraOffsetStore.Queries(system)
 
-  val sessionWrapper = DefaultJournalCassandraSession(system, "app-session", queries.init)
+  val sessionWrapper =
+    DefaultJournalCassandraSession(system, "app-session", queries.init)
 
   val offsetStore = new CassandraOffsetStore(sessionWrapper, queries)
 
   val journal = AggregateJournal(system, offsetStore)
 
   val authorizationRegion =
-    AggregateSharding(system).start[CardAuthorization.Command](CardAuthorization())
+    AggregateSharding(system)
+      .start[CardAuthorization.Command](CardAuthorization())
 
   val accountRegion =
-    AggregateSharding(system).start[AccountAggregateOp](AccountAggregate(Clock.systemUTC()))
+    AggregateSharding(system)
+      .start[AccountAggregateOp](AccountAggregate(Clock.systemUTC()))
 
   val scheduleEntityName = "Schedule3"
 
-  val schedule: Schedule = Schedule(system, scheduleEntityName, 1.day, 10.seconds, offsetStore)
-
+  val schedule: Schedule =
+    Schedule(system, scheduleEntityName, 1.day, 10.seconds, offsetStore)
 
   val cardAuthorizationEventStream =
-    new DefaultEventStream(system, journal.committableEventSourceFor[CardAuthorization]("CardAuthorization-API").map(_.value))
+    new DefaultEventStream(
+      system,
+      journal
+        .committableEventSourceFor[CardAuthorization]("CardAuthorization-API")
+        .map(_.value))
 
-
-  val authorizePaymentAPI = new AuthorizePaymentAPI(authorizationRegion, cardAuthorizationEventStream, Logging(system, classOf[AuthorizePaymentAPI]))
+  val authorizePaymentAPI = new AuthorizePaymentAPI(
+    authorizationRegion,
+    cardAuthorizationEventStream,
+    Logging(system, classOf[AuthorizePaymentAPI]))
   val accountApi = new AccountAPI(accountRegion)
 
   val accountInterpreter = new (AccountAggregateOp ~> Future) {
-    override def apply[A](fa: AccountAggregateOp[A]): Future[A] = accountRegion.ask(fa)
+    override def apply[A](fa: AccountAggregateOp[A]): Future[A] =
+      accountRegion.ask(fa)
   }
-  val cardAuthorizationInterpreter = new (CardAuthorization.Command ~> Future) {
-    override def apply[A](fa: CardAuthorization.Command[A]): Future[A] = authorizationRegion.ask(fa)
-  }
+  val cardAuthorizationInterpreter =
+    new (CardAuthorization.Command ~> Future) {
+      override def apply[A](fa: CardAuthorization.Command[A]): Future[A] =
+        authorizationRegion.ask(fa)
+    }
 
   import freek._
 
   val interpreter = accountInterpreter :&: cardAuthorizationInterpreter
 
   journal
-  .committableEventSourceFor[CardAuthorization]("processing")
-  .collect {
-    case CommittableJournalEntry(offset, _, _, e: CardAuthorizationCreated) =>
-      (e, offset)
-  }
-  .via(AuthorizationProcess.flow(8, interpreter))
-  .mapAsync(1)(_.commitScaladsl())
-  .runWith(Sink.ignore)
-
+    .committableEventSourceFor[CardAuthorization]("processing")
+    .collect {
+      case CommittableJournalEntry(offset,
+                                   _,
+                                   _,
+                                   e: CardAuthorizationCreated) =>
+        (e, offset)
+    }
+    .via(AuthorizationProcess.flow(8, interpreter))
+    .mapAsync(1)(_.commitScaladsl())
+    .runWith(Sink.ignore)
 
   val route = path("check") {
     get {
       complete(StatusCodes.OK)
     }
   } ~
-    authorizePaymentAPI.route ~
-    accountApi.route
+      AuthorizePaymentAPI.route(authorizePaymentAPI) ~
+      AccountAPI.route(accountApi)
 
-  Http().bindAndHandle(route, config.getString("http.interface"), config.getInt("http.port"))
-  .onComplete { result => log.info("Bind result [{}]", result) }
+  Http()
+    .bindAndHandle(route,
+                   config.getString("http.interface"),
+                   config.getInt("http.port"))
+    .onComplete { result =>
+      log.info("Bind result [{}]", result)
+    }
 
 }
