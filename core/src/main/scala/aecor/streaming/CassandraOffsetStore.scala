a9e9cb0751291d6496989398d3e386e482cc1ffa
Denis Mikhaylov <notxcain@gmail.com>
2016-12-19 22:28:11 +0300

    Make config final

--- core/src/main/scala/aecor/streaming/CassandraOffsetStore.scala
+++ core/src/main/scala/aecor/streaming/CassandraOffsetStore.scala
@@ -4,11 +4,11 @@ import java.util.UUID
 
 import akka.persistence.cassandra.session.scaladsl.CassandraSession
 import com.datastax.driver.core.Session
-
+import aecor.util.cassandra._
 import scala.concurrent.{ ExecutionContext, Future }
 
 object CassandraOffsetStore {
-  case class Config(keyspace: String, tableName: String = "consumer_offset") {
+  final case class Config(keyspace: String, tableName: String = "consumer_offset") {
     def createTableQuery: String =
       s"CREATE TABLE IF NOT EXISTS $keyspace.$tableName (consumer_id text, tag text, offset uuid, PRIMARY KEY ((consumer_id, tag)))"
     def updateOffsetQuery: String =
@@ -17,12 +17,9 @@ object CassandraOffsetStore {
       s"SELECT offset FROM $keyspace.$tableName WHERE consumer_id = ? AND tag = ?"
   }
 
-  def createTable(
-    config: Config
-  )(implicit executionContext: ExecutionContext): Session => Future[Unit] = { session =>
-    import aecor.util.cassandra._
-    session.executeAsync(config.createTableQuery).map(_ => ())
-  }
+  def createTable(config: Config)(
+    implicit executionContext: ExecutionContext
+  ): Session => Future[Unit] = _.executeAsync(config.createTableQuery).map(_ => ())
 
   def apply(session: CassandraSession, config: CassandraOffsetStore.Config)(
     implicit executionContext: ExecutionContext
