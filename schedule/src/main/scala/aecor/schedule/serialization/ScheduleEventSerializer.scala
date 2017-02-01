d867d98c7aae69d3864218ecbb0e30dce776860f
Denis Mikhaylov <notxcain@gmail.com>
2017-02-01 18:21:08 +0300

    Fix Schedule Event serialization bug (#13)

---
+++ schedule/src/main/scala/aecor/schedule/serialization/ScheduleEventSerializer.scala
@@ -0,0 +1,11 @@
+package aecor.schedule.serialization
+
+import aecor.aggregate.serialization.PersistentReprSerializer
+import akka.actor.ExtendedActorSystem
+
+/**
+  * Serializer with id = 120 for backwards compatibilty
+  *
+  * @param system Actor system
+  */
+class ScheduleEventSerializer(system: ExtendedActorSystem) extends PersistentReprSerializer(system)
