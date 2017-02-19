0469f9fe1fd41bd6c8efa64f93fbb9b9f024639b
Denis Mikhaylov <notxcain@gmail.com>
2017-02-19 17:13:45 +0300

    wip

--- core/src/main/scala/aecor/aggregate/runtime/behavior.scala
+++ core/src/main/scala/aecor/aggregate/runtime/behavior.scala
@@ -3,6 +3,15 @@ package aecor.aggregate.runtime
 import cats.~>
 
 object behavior {
+
+  /**
+    * A transformer type representing a Tuple2 wrapped in [F]
+    */
   type Tuple2T[F[_], A, B] = F[(A, B)]
+
+  /**
+    * `Behavior[Op, F]` says that each operation `Op[A]` will cause effect `F`
+    * producing a tuple consisting of next `Behavior[Op, F]` and an `A`
+    */
   final case class Behavior[Op[_], F[_]](run: Op ~> Tuple2T[F, Behavior[Op, F], ?])
 }
