fb0b1ba1ed547d39aa226e4f363026d708bdca94
Denis Mikhaylov <notxcain@gmail.com>
2018-03-28 16:56:18 +0300

    Add EntityCommand serializer

--- modules/akka-persistence-runtime/src/main/scala/aecor/runtime/akkapersistence/serialization/MessageSerializer.scala
+++ modules/akka-persistence-runtime/src/main/scala/aecor/runtime/akkapersistence/serialization/MessageSerializer.scala
@@ -2,9 +2,11 @@ package aecor.runtime.akkapersistence.serialization
 
 import java.nio.ByteBuffer
 
+import aecor.runtime.akkapersistence.AkkaPersistenceRuntime.EntityCommand
 import aecor.runtime.akkapersistence.AkkaPersistenceRuntimeActor.HandleCommand
 import akka.actor.ExtendedActorSystem
 import akka.serialization.{ BaseSerializer, SerializerWithStringManifest }
+import com.google.protobuf.ByteString
 
 import scala.collection.immutable._
 
@@ -13,18 +15,25 @@ class MessageSerializer(val system: ExtendedActorSystem)
     with BaseSerializer {
 
   val HandleCommandManifest = "A"
+  val EntityCommandManifest = "B"
 
   private val fromBinaryMap =
-    HashMap[String, Array[Byte] ⇒ AnyRef](HandleCommandManifest -> handleCommandFromBinary)
+    HashMap[String, Array[Byte] ⇒ AnyRef](
+      HandleCommandManifest -> handleCommandFromBinary,
+      EntityCommandManifest -> entityCommandFromBinary
+    )
 
   override def manifest(o: AnyRef): String = o match {
-    case HandleCommand(_) => HandleCommandManifest
-    case x                => throw new IllegalArgumentException(s"Serialization of [$x] is not supported")
+    case HandleCommand(_)    => HandleCommandManifest
+    case EntityCommand(_, _) => EntityCommandManifest
+    case x                   => throw new IllegalArgumentException(s"Serialization of [$x] is not supported")
   }
 
   override def toBinary(o: AnyRef): Array[Byte] = o match {
     case x @ HandleCommand(_) =>
       x.commandBytes.array()
+    case x @ EntityCommand(_, _) =>
+      entityCommandToBinary(x)
     case x => throw new IllegalArgumentException(s"Serialization of [$x] is not supported")
   }
 
@@ -32,6 +41,15 @@ class MessageSerializer(val system: ExtendedActorSystem)
     fromBinaryMap.get(manifest) match {
       case Some(f) => f(bytes)
       case other   => throw new IllegalArgumentException(s"Unknown manifest [$other]")
+    }
+
+  private def entityCommandToBinary(a: EntityCommand): Array[Byte] =
+    msg.EntityCommand(a.entityKey, ByteString.copyFrom(a.commandBytes)).toByteArray
+
+  private def entityCommandFromBinary(bytes: Array[Byte]): EntityCommand =
+    msg.EntityCommand.parseFrom(bytes) match {
+      case msg.EntityCommand(entityId, commandBytes) =>
+        EntityCommand(entityId, commandBytes.asReadOnlyByteBuffer)
     }
 
   private def handleCommandFromBinary(bytes: Array[Byte]): HandleCommand =
