02802bbb34cd2b3b83a9de8012bf645c4c3de71f
Denis Mikhaylov <notxcain@gmail.com>
2018-04-26 20:37:43 +0300

    Fix serialization config

--- modules/akka-cluster-runtime/src/main/scala/aecor/runtime/akkageneric/serialization/Message.scala
+++ modules/akka-cluster-runtime/src/main/scala/aecor/runtime/akkageneric/serialization/Message.scala
@@ -3,4 +3,4 @@ package aecor.runtime.akkageneric.serialization
 /**
   * Marker trait for all protobuf-serializable messages in `aecor.runtime.akkageneric`.
   */
-private[aecor] trait Message
+private[akkageneric] trait Message
