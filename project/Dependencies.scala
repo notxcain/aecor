b3a28bd87741f7ba45177ae605947edbc47bc87b
SemanticBeeng <nick@semanticbeeng.com>
2018-11-19 03:13:15 -0800

    1. Minor build cleanup 2. Moving example out to separate `apps` project.

---
+++ project/Dependencies.scala
@@ -0,0 +1,36 @@
+import sbt.AutoPlugin
+
+
+object Dependencies extends AutoPlugin {
+
+  object versions {
+    lazy val akka = "2.5.15"
+    lazy val akkaPersistenceCassandra = "0.61"
+
+    lazy val cats = "1.4.0"
+    lazy val catsEffect = "1.0.0"
+    lazy val scodec = "1.10.4"
+    lazy val logback = "1.1.7"
+    lazy val cassandraDriverExtras = "3.1.0"
+    lazy val jsr305 = "3.0.1"
+    lazy val boopickle = "1.3.0"
+    lazy val monocle = "1.5.0-cats"
+    lazy val fs2 = "1.0.0"
+    lazy val log4cats = "0.2.0-M1"
+
+    lazy val scalaCheck = "1.13.4"
+    lazy val scalaTest = "3.0.5"
+    lazy val scalaCheckShapeless = "1.1.4"
+    lazy val shapeless = "2.3.3"
+    lazy val kindProjector = "0.9.7"
+    lazy val scalameta = "1.8.0"
+
+    // Example dependencies
+
+    lazy val circe = "0.9.3"
+    lazy val scalametaParadise = "3.0.0-M10"
+
+    lazy val catsTagless = "0.1.0"
+
+  }
+}
