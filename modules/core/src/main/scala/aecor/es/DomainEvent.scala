2000960d35bb7d104345ba740bdade2dce191373
SemanticBeeng <nick@semanticbeeng.com>
2018-12-08 09:20:44 -0800

    wip

--- modules/core/src/main/scala/aecor/es/DomainEvent.scala
+++
@@ -1,6 +0,0 @@
-package aecor.es
-
-/**
-  * Market trait
-  */
-trait DomainEvent extends Product with Serializable
--- modules/core/src/main/scala/aecor/es/DomainState.scala
+++
@@ -1,6 +0,0 @@
-package aecor.es
-
-/**
-  * Market trait
-  */
-trait DomainState extends Product with Serializable
