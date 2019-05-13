92acbc15a1dd881009c18676ea64cb1c857d591a
Denis Mikhaylov <notxcain@gmail.com>
2019-05-13 15:23:20 +0300

    wip

--- modules/tests/src/test/scala/aecor/tests/LawSuite.scala
+++ modules/tests/src/test/scala/aecor/tests/LawSuite.scala
@@ -1,62 +1,8 @@
 package aecor.tests
 
-import catalysts.Platform
 import cats.Eq
-import cats.instances.AllInstances
-import cats.syntax.{ AllSyntax, EqOps }
-import org.scalactic.{ CanEqual, Equality, Equivalence, TripleEquals }
 import org.scalactic.TripleEqualsSupport.{ AToBEquivalenceConstraint, BToAEquivalenceConstraint }
-import org.scalactic.anyvals.{ PosInt, PosZDouble, PosZInt }
-import org.scalatest.{ FunSuite, FunSuiteLike, Matchers }
-import org.scalatest.prop.{ Configuration, GeneratorDrivenPropertyChecks }
-import org.typelevel.discipline.scalatest.Discipline
-
-trait TestSettings extends Configuration with Matchers {
-
-  lazy val checkConfiguration: PropertyCheckConfiguration =
-    PropertyCheckConfiguration(
-      minSuccessful = if (Platform.isJvm) PosInt(50) else PosInt(5),
-      maxDiscardedFactor = if (Platform.isJvm) PosZDouble(5.0) else PosZDouble(50.0),
-      minSize = PosZInt(0),
-      sizeRange = if (Platform.isJvm) PosZInt(10) else PosZInt(5),
-      workers = PosInt(1)
-    )
-
-  lazy val slowCheckConfiguration: PropertyCheckConfiguration =
-    if (Platform.isJvm) checkConfiguration
-    else PropertyCheckConfiguration(minSuccessful = 1, sizeRange = 1)
-}
-
-/**
-  * An opinionated stack of traits to improve consistency and reduce
-  * boilerplate. Copied from Cats library.
-  */
-trait LawSuite
-    extends FunSuite
-    with Matchers
-    with GeneratorDrivenPropertyChecks
-    with Discipline
-    with TestSettings
-    with AllInstances
-    with AllSyntax
-    with StrictCatsEquality { self: FunSuiteLike =>
-
-  implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
-    checkConfiguration
-
-  // disable Eq syntax (by making `catsSyntaxEq` not implicit), since it collides
-  // with scalactic's equality
-  override def catsSyntaxEq[A: Eq](a: A): EqOps[A] = new EqOps[A](a)
-
-  def even(i: Int): Boolean = i % 2 == 0
-
-  val evenPf: PartialFunction[Int, Int] = { case i if even(i) => i }
-}
-
-trait SlowLawSuite extends LawSuite {
-  implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
-    slowCheckConfiguration
-}
+import org.scalactic.{ CanEqual, Equality, Equivalence, TripleEquals }
 
 final class CatsEquivalence[T](T: Eq[T]) extends Equivalence[T] {
   def areEquivalent(a: T, b: T): Boolean = T.eqv(a, b)
--- modules/tests/src/test/scala/aecor/tests/ScheduleBucketSpec.scala
+++ modules/tests/src/test/scala/aecor/tests/ScheduleBucketSpec.scala
@@ -2,16 +2,18 @@ package aecor.tests
 
 import java.time._
 
-import aecor.data.{ ActionT, Folded}
-import aecor.schedule.ScheduleEvent.{ScheduleEntryAdded, ScheduleEntryFired}
-import aecor.schedule.{DefaultScheduleBucket, ScheduleEvent, ScheduleState}
+import aecor.data.{ ActionT, Folded }
+import aecor.schedule.ScheduleEvent.{ ScheduleEntryAdded, ScheduleEntryFired }
+import aecor.schedule.{ DefaultScheduleBucket, ScheduleEvent, ScheduleState }
 import cats.Id
 import cats.data.Chain
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{ FlatSpec, Matchers }
 
 class ScheduleBucketSpec extends FlatSpec with Matchers with StrictCatsEquality {
   val clock = Clock.fixed(Instant.now, ZoneId.systemDefault())
-  val aggregate = DefaultScheduleBucket[ActionT[Id, ScheduleState, ScheduleEvent, ?], Id](ZonedDateTime.now(clock))
+  val aggregate = DefaultScheduleBucket[ActionT[Id, ScheduleState, ScheduleEvent, ?], Id](
+    ZonedDateTime.now(clock)
+  )
 
   "ScheduleBucket" should "fire entry when due date is before now" in {
     val handler = aggregate.addScheduleEntry(
@@ -21,31 +23,35 @@ class ScheduleBucketSpec extends FlatSpec with Matchers with StrictCatsEquality 
     )
 
     val Folded.Next((events, _)) = handler.run(ScheduleState.initial, _.update(_))
-    assert(events ===
-      Chain(
-        ScheduleEntryAdded(
-          "entryId",
-          "correlation",
-          LocalDateTime.now(clock).minusSeconds(10),
-          Instant.now(clock)
-        ),
-        ScheduleEntryFired("entryId", "correlation", Instant.now(clock))
-      ))
+    assert(
+      events ===
+        Chain(
+          ScheduleEntryAdded(
+            "entryId",
+            "correlation",
+            LocalDateTime.now(clock).minusSeconds(10),
+            Instant.now(clock)
+          ),
+          ScheduleEntryFired("entryId", "correlation", Instant.now(clock))
+        )
+    )
   }
   it should "not fire entry when due date is after now" in {
     val handler =
       aggregate.addScheduleEntry("entryId", "correlation", LocalDateTime.now(clock).plusSeconds(10))
 
     val Folded.Next((events, _)) = handler.run(ScheduleState.initial, _.update(_))
-    assert(events ===
-      Chain(
-        ScheduleEntryAdded(
-          "entryId",
-          "correlation",
-          LocalDateTime.now(clock).plusSeconds(10),
-          Instant.now(clock)
+
+    assert(
+      events ===
+        Chain(
+          ScheduleEntryAdded(
+            "entryId",
+            "correlation",
+            LocalDateTime.now(clock).plusSeconds(10),
+            Instant.now(clock)
+          )
         )
-      )
     )
   }
 }
