3cfbad2edfc628f20a7807e7b64eec105da2b3ee
Denis Mikhaylov <notxcain@gmail.com>
2019-05-13 15:31:33 +0300

    wip

--- modules/core/src/main/scala/aecor/encoding/WireProtocol.scala
+++ modules/core/src/main/scala/aecor/encoding/WireProtocol.scala
@@ -1,19 +1,18 @@
 package aecor.encoding
 
 import aecor.data.PairE
-import aecor.encoding.WireProtocol.DecoderK
+import aecor.encoding.WireProtocol.Invocation
 import scodec.bits.BitVector
 import scodec.{ Decoder, Encoder }
 
 trait WireProtocol[M[_[_]]] {
-  def decoder: DecoderK[M]
+  def decoder: Decoder[PairE[Invocation[M, ?], Encoder]]
   def encoder: M[WireProtocol.Encoded]
 }
 
 object WireProtocol {
   def apply[M[_[_]]](implicit M: WireProtocol[M]): WireProtocol[M] = M
   type Encoded[A] = (BitVector, Decoder[A])
-  type DecoderK[M[_[_]]] = Decoder[PairE[Invocation[M, ?], Encoder]]
   trait Invocation[M[_[_]], A] {
     def run[F[_]](mf: M[F]): F[A]
   }
