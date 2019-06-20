d7b4a30e807755755ce599deab165fbd2a01fe74
Jisoo Park <jisoo.park@doomoolmori.com>
2019-06-20 19:10:30 +0900

    Fix ScheduleEventCodecSpec

--- modules/tests/src/test/scala/aecor/tests/ScheduleEventCodecSpec.scala
+++ modules/tests/src/test/scala/aecor/tests/ScheduleEventCodecSpec.scala
@@ -1,6 +1,6 @@
 package aecor.tests
 
-import java.time.temporal.ChronoField
+import java.time.temporal.{ChronoField, Temporal}
 import java.time.{Instant, LocalDateTime}
 
 import aecor.runtime.akkapersistence.serialization.{PersistentDecoder, PersistentEncoder}
@@ -13,12 +13,13 @@ class ScheduleEventCodecSpec extends AkkaSpec with PropertyChecks {
   val encoder = PersistentEncoder[ScheduleEvent]
   val decoder = PersistentDecoder[ScheduleEvent]
 
-  implicit val arbitraryLocalDateTime = Arbitrary(Gen.lzy(Gen.const(LocalDateTime.now())))
-
   // OpenJDK 9+ offers more precise system clock than millisecond.
   // https://bugs.openjdk.java.net/browse/JDK-8068730
-  implicit val arbitraryInstant =
-    Arbitrary(Gen.lzy(Gen.const(Instant.now().`with`(ChronoField.MICRO_OF_SECOND, 0L))))
+  def dropBelowMillis[A <: Temporal](t: A): A =
+    t.`with`(ChronoField.MICRO_OF_SECOND, t.getLong(ChronoField.MILLI_OF_SECOND) * 1000L).asInstanceOf[A]
+
+  implicit val arbitraryLocalDateTime = Arbitrary(Gen.lzy(Gen.const(dropBelowMillis(LocalDateTime.now()))))
+  implicit val arbitraryInstant       = Arbitrary(Gen.lzy(Gen.const(dropBelowMillis(Instant.now()))))
 
   "ScheduleEventCodec" must {
     "be able to encode and decode ScheduleEvent" in {
