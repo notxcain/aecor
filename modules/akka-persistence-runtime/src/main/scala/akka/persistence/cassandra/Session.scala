bff8e833c0f11ed99f40e9f56fe2f211670d4f22
Denis Mikhaylov <notxcain@gmail.com>
2019-07-02 14:25:37 +0300

    wip

--- modules/akka-persistence-runtime/src/main/scala/akka/persistence/cassandra/Session.scala
+++ modules/akka-persistence-runtime/src/main/scala/akka/persistence/cassandra/Session.scala
@@ -2,9 +2,11 @@ package akka.persistence.cassandra
 import java.util.concurrent.Executor
 
 import cats.data.Kleisli
-import cats.effect.Async
+import cats.effect.{ Async, ContextShift }
 import com.datastax.driver.core.{ ResultSet, TypeCodec, Session => DatastaxSession }
+import cats.effect.implicits._
 
+import scala.concurrent.ExecutionContext
 import scala.util.control.NonFatal
 
 trait Session[F[_]] {
@@ -14,26 +16,32 @@ trait Session[F[_]] {
 
 object Session {
   type Init[F[_]] = Kleisli[F, Session[F], Unit]
+  def Init[F[_]](f: Session[F] => F[Unit]): Init[F] = Kleisli(f)
   private val immediateExecutor = new Executor {
     override def execute(command: Runnable): Unit =
       command.run()
   }
 
-  def apply[F[_]](datastaxSession: DatastaxSession)(implicit F: Async[F]): Session[F] =
+  private val immediateExecutionContext = ExecutionContext.fromExecutor(immediateExecutor)
+
+  def apply[F[_]](datastaxSession: DatastaxSession)(implicit F: Async[F],
+                                                    contextShift: ContextShift[F]): Session[F] =
     new Session[F] {
       final override def execute(query: String): F[ResultSet] =
-        F.async { cb =>
-          val future = datastaxSession.executeAsync(query)
-          val runnable = new Runnable {
-            override def run(): Unit =
-              try {
-                cb(Right(future.get()))
-              } catch {
-                case NonFatal(e) =>
-                  cb(Left(e))
-              }
+        contextShift.evalOn(immediateExecutionContext) {
+          F.async { cb =>
+            val future = datastaxSession.executeAsync(query)
+            val runnable = new Runnable {
+              override def run(): Unit =
+                try {
+                  cb(Right(future.get()))
+                } catch {
+                  case NonFatal(e) =>
+                    cb(Left(e))
+                }
+            }
+            future.addListener(runnable, immediateExecutor)
           }
-          future.addListener(runnable, immediateExecutor)
         }
       override def registerCodec[A](codec: TypeCodec[A]): F[Unit] =
         F.delay {
